9ab2f755da1aa9dba38a167c1779f67e31424b12
Batta <Gianlucabb1@gmail.com>
2024-08-31 19:49:58 -0300

    lo que el dijo
    lo que el dijo por dos

---
+++ Progra2/Assets/Test_Obj & Cam/Scripts/FacingCam.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FacingCam : MonoBehaviour
+{
+    [SerializeField] Transform _camera;
+    Vector3 _dir = new();
+    // Update is called once per frame
+    void Update()
+    {
+        transform.up = -_camera.forward;
+    }
+}
