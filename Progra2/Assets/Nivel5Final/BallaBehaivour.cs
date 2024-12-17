b0cdf27438ce77bd4f1788521f1ffbf0587f00be
Batta <Gianlucabb1@gmail.com>
2024-12-17 10:08:59 -0300

    Balas "bien"

--- Progra2/Assets/Nivel5Final/BallaBehaivour.cs
+++ Progra2/Assets/Nivel5Final/BallaBehaivour.cs
@@ -7,16 +7,41 @@ public class BallaBehaivour : MonoBehaviour
 {
     Rigidbody rb;
     [SerializeField] float speed;
+    Vector3 dir;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        Destroy(gameObject, 3f);
     }
 
 
-    void Update()
+    void FixedUpdate()
     {
-        rb.AddForce(transform.forward * speed * Time.deltaTime);
+        Go();
     }
-    
+
+    void Go()
+    {
+        if (dir == new Vector3(0,0,0))
+        {
+            dir = Camera.main.ScreenPointToRay(Input.mousePosition).direction;
+        }
+        else
+        {
+            rb.AddForce(dir * speed * Time.fixedDeltaTime, ForceMode.Impulse);
+        }
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+
+        if (collision != null && collision.gameObject.GetComponent<BallaBehaivour>() == null)
+        {
+            print("choco con " + collision.gameObject.name);
+            Destroy(gameObject);
+        }
+    }
+
 }
--- Progra2/Assets/Nivel5Final/Pew Pew.cs
+++ Progra2/Assets/Nivel5Final/Pew Pew.cs
@@ -1,29 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class PewPew : MonoBehaviour
 {
+    [SerializeField] bool semi = true;
     [SerializeField] GameObject bullet;
     [SerializeField] GameObject shootHere;
-
-    void Start()
-    {
-
-    }
 
 
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (semi)
         {
-            Shoot();
+            if (Input.GetMouseButtonDown(0))
+            {
+                Shoot();
+            }
         }
+        else
+        {
+            if (Input.GetMouseButton(0))
+            {
+                Shoot();
+            }
+        }
+
     }
 
     void Shoot()
     {
-        print("piu piu");
-        Instantiate(bullet, shootHere.transform);
+        Instantiate(bullet, shootHere.transform.position, quaternion.identity);
+
     }
 }
